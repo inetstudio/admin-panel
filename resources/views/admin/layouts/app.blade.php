3242d6fbb582b4851da4ff6ebd37a30e4f4878ba
Konstantin Bukin <bukin@inetstudio.ru>
2017-05-28 01:12:36 +0300

    Add csrf meta

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
         <title>Admin | @yield('title') </title>
 
         <link rel="icon" href="{!! asset('images/favicon.ico') !!}">
