c378a369fab87e3c3967fab9ea166e024782b87b
Konstantin Bukin <bukin@inetstudio.ru>
2017-12-02 17:43:47 +0300

    Refactoring JSON trait

--- src/Models/ACL/UserProfileModel.php
+++ src/Models/ACL/UserProfileModel.php
@@ -5,10 +5,12 @@ namespace InetStudio\AdminPanel\Models\ACL;
 use App\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use InetStudio\AdminPanel\Models\Traits\HasJSONColumns;
 
 class UserProfileModel extends Model
 {
     use SoftDeletes;
+    use HasJSONColumns;
 
     /**
      * Связанная с моделью таблица.
@@ -54,59 +56,5 @@ class UserProfileModel extends Model
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
-    }
-
-    /*
-     * Determine if the media item has a custom property with the given name.
-     */
-    public function hasAdditionalInfo(string $propertyName): bool
-    {
-        return array_has($this->additional_info, $propertyName);
-    }
-
-    /**
-     * Get if the value of additional info with the given name.
-     *
-     * @param string $propertyName
-     * @param mixed $default
-     *
-     * @return mixed
-     */
-    public function getAdditionalInfo(string $propertyName, $default = null)
-    {
-        return array_get($this->additional_info, $propertyName, $default);
-    }
-
-    /**
-     * @param string $name
-     * @param mixed $value
-     *
-     * @return $this
-     */
-    public function setAdditionalInfo(string $name, $value)
-    {
-        $additionalInfo = $this->additional_info;
-
-        array_set($additionalInfo, $name, $value);
-
-        $this->additional_info = $additionalInfo;
-
-        return $this;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return $this
-     */
-    public function forgetAdditionalInfo(string $name)
-    {
-        $additionalInfo = $this->additional_info;
-
-        array_forget($additionalInfo, $name);
-
-        $this->additional_info = $additionalInfo;
-
-        return $this;
     }
 }
