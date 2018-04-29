8880e5dcc3f012e5714a00d6bc59b26389185ba6
Konstantin Bukin <bukin@inetstudio.ru>
2018-04-30 00:43:27 +0300

    Add helpers

---
+++ src/panel/helpers.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Blade;
+
+if (! function_exists('static_asset')) {
+    function static_asset($asset)
+    {
+        if (! config('app.static_url'))
+            return asset($asset);
+
+        $static = config('app.static_url');
+
+        return static_path($static, $asset).(env('RELEASE_VERSION') ? '?v='.env('RELEASE_VERSION') : '');
+    }
+}
+
+if (! function_exists('static_path')) {
+    function static_path($static, $asset)
+    {
+        return $static . "/" . ltrim($asset, "/");
+    }
+}
+
+if (! function_exists('blade_string')) {
+    function blade_string($value, array $args = array())
+    {
+        $generated = Blade::compileString($value);
+
+        ob_start() and extract($args, EXTR_SKIP);
+
+        try {
+            eval('?>'.$generated);
+        } catch (\Exception $e) {
+            ob_get_clean(); throw $e;
+        }
+
+        $content = ob_get_clean();
+
+        return $content;
+    }
+}
