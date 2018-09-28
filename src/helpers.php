99d9537f3d92b2f5928f9839608324ab8f0d3d66
Konstantin Bukin <bukin@inetstudio.ru>
2018-09-28 03:13:01 +0300

    Add string helper

--- src/helpers.php
+++ src/helpers.php
@@ -39,3 +39,25 @@ if (! function_exists('blade_string')) {
         return $content;
     }
 }
+
+if (! function_exists('get_correct_word')) {
+    function get_correct_word($num, array $variants)
+    {
+        $num = abs($num);
+        $val = $num % 100;
+
+        if ($val > 10 && $val < 20) {
+            return $variants[2];
+        } else {
+            $val = $num % 10;
+
+            if ($val == 1) {
+                return $variants[0];
+            } elseif ($val > 1 && $val < 5) {
+                return $variants[1];
+            } else {
+                return $variants[2];
+            }
+        }
+    }
+}
