ea7e516ef92b77663a1f2cf17b382f9f88baed58
Konstantin Bukin <bukin@inetstudio.ru>
2019-04-18 09:56:26 +0300

    Create SlugsServiceTrait.php

---
+++ entities/base/src/Services/Traits/SlugsServiceTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace InetStudio\AdminPanel\Base\Services\Traits;
+
+/**
+ * Trait SlugsServiceTrait.
+ */
+trait SlugsServiceTrait
+{
+    /**
+     * Получаем объект по slug.
+     *
+     * @param string $slug
+     * @param array $params
+     *
+     * @return mixed
+     */
+    public function getItemBySlug(string $slug, array $params = [])
+    {
+        return $this->model->itemBySlug($slug, $params)->first();
+    }
+}
