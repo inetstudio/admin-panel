b5f680fa7431356f6bf703776e8c66fffb21fd39
Konstantin Bukin <bukin@inetstudio.ru>
2020-09-12 11:08:43 +0300

    Add mask to input component

--- resources/assets/js/components/fields/BaseInputText.vue
+++ resources/assets/js/components/fields/BaseInputText.vue
@@ -3,7 +3,7 @@
         <div class="form-group row" :class="{'has-error': hasError}">
             <label :for="name" class="col-sm-2 col-form-label font-bold">{{ label }}</label>
             <div class="col-sm-10">
-                <input :name="name" type="text" :value="value" :id="name" class="form-control" v-bind="attributes" @focus="$emit('focus')" @input="$emit('update:value', $event.target.value)">
+                <input :name="name" type="text" :value="value" :id="name" class="form-control" v-bind="attributes" @focus="$emit('focus')" @input="$emit('update:value', $event.target.value)" ref="field">
 
                 <span class="form-text m-b-none"
                       v-for = "(error, index) in fieldErrors"
@@ -16,6 +16,8 @@
 </template>
 
 <script>
+    import IMask from 'imask';
+
     export default {
         name: 'BaseInputText',
         props: {
@@ -37,11 +39,24 @@
                 default() {
                     return {};
                 }
+            },
+            options: {
+              type: Object,
+              default() {
+                return {};
+              }
             }
         },
         mixins: [
             window.Admin.vue.mixins['errors']
-        ]
+        ],
+        mounted() {
+          let component = this;
+
+          if (_.has(component.options, 'mask')) {
+            IMask(component.$refs.field, component.options.mask);
+          }
+        }
     }
 </script>
 
